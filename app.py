497b793a1f9e75cfd3726839ad7d0f37623daefe
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2023-04-11 19:02:12 -0300

    feat: app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask, jsonify, request
 from mysql.connector import connect, Error
+
+
+try:
+    connection = connect(
+        host="localhost",
+        user="root",
+        password=""
+
+    )
